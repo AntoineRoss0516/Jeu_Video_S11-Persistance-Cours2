2a76db91a10c3badbf27a11e984130f43f1909f1
AntoineRoss0516 <antoine.ross.16@outlook.com>
2025-04-25 15:43:31 -0400

    Save

--- Assets/Scripts/PortailV2.cs
+++ Assets/Scripts/PortailV2.cs
@@ -6,11 +6,20 @@ public class PortailV2 : MonoBehaviour
 {
     public int indexSceneACharger;
 
+    public GameObject saveClick;
     private void OnTriggerEnter2D(Collider2D autre)
     {
         if (autre.CompareTag("Joueur"))
         {
             JeuEspaceV2.instance.ChangerScene(indexSceneACharger);
+            JeuEspaceV2.instance.JouerSonPortail();
+    
+
+         
+        
+        // Si on a d�j� un singleton pr�t
+      
+            
         }
     }
 }
